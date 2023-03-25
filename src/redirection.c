979427745292f18d9e5c53a97297687c2983f4ac
Daneality <dgolmov@gmail.com>
2023-03-25 15:27:50 +0100

    dsfdsf

--- src/redirection.c
+++ src/redirection.c
@@ -6,7 +6,7 @@
 /*   By: dsas <dsas@student.42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 14:19:38 by dsas              #+#    #+#             */
-/*   Updated: 2023/03/24 19:55:12 by dsas             ###   ########.fr       */
+/*   Updated: 2023/03/25 15:27:31 by dsas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,34 @@ void	skip_space(t_token **token)
 		*token = (*token)->next;
 }
 
-void	here_doc(t_token **token, t_token **token_tmp,
-					t_pipe_group **tmp, t_pipe_group **pipes)
+char	*here_doc_init(char **file_name, t_token **token_tmp,
+						t_pipe_group **tmp, int *file)
+{
+	char	*limiter;
+
+	*file_name = ft_strdup(".here_doc");
+	charjoin_free(file_name, (*tmp)->pipe_index + '0');
+	limiter = (*token_tmp)->info;
+	*file = open(*file_name, O_CREAT | O_WRONLY | O_TRUNC, 0000644);
+	return (limiter);
+}
+
+void	here_doc_finish(t_token **token_tmp,
+						t_pipe_group **tmp, char **file_name)
+{
+	(*tmp)->input = open(*file_name, O_RDONLY);
+	(*tmp)->heredoc = *file_name;
+	*token_tmp = (*token_tmp)->next;
+}
+
+void	here_doc(t_token **token_tmp, t_pipe_group **tmp)
 {
 	int		file;
 	char	*buf;
 	char	*limiter;
 	char	*file_name;
 
-	file_name = ft_strdup(".here_doc");
-	charjoin_free(&file_name, (*tmp)->pipe_index + '0');
-	limiter = (*token_tmp)->info;
-	file = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0000644);
+	limiter = here_doc_init(&file_name, token_tmp, tmp, &file);
 	if (file < 0)
 	{
 		throw_error("minishell: couldn't open HEREDOC\n");
@@ -49,7 +65,5 @@ void	here_doc(t_token **token, t_token **token_tmp,
 	}
 	free(buf);
 	close(file);
-	(*tmp)->input = open(file_name, O_RDONLY);
-	(*tmp)->heredoc = file_name;
-	*token_tmp = (*token_tmp)->next;
+	here_doc_finish(token_tmp, tmp, &file_name);
 }
